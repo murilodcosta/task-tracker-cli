078caaa8f210e4cec3fb988232b73204eb0d3f7c
murilodcosta <murilodiasdacosta@gmail.com>
2026-03-31 21:28:55 -0300

    feat: refactor Task and create TaskManager

---
+++ src/main/java/com/murilodcosta/tasktrackercli/manager/TaskManager.java
@@ -0,0 +1,105 @@
+package com.murilodcosta.tasktrackercli.manager;
+
+import com.murilodcosta.tasktrackercli.model.Task;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class TaskManager {
+    private final Path FILE_PATH = Path.of("tasks.json");
+    private List<Task> tasks;
+
+    public TaskManager() {
+        this.tasks = loadTasks();
+    }
+
+    /* read tasks.json file */
+    private List<Task> loadTasks(){
+        List<Task> storedTasks = new ArrayList<>();
+
+        if (!Files.exists(FILE_PATH)){
+            return new ArrayList<>();
+        }
+
+        try {
+            String jsonContent = Files.readString(FILE_PATH);
+            String[] taskList = jsonContent.replace("[", "")
+                    .replace("]", "")
+                    .split("},");
+            for (String taskJson : taskList){
+                if (!taskJson.endsWith("}")){
+                    taskJson = taskJson + "}";
+                    storedTasks.add(Task.fromJson(taskJson));
+                } else {
+                    storedTasks.add(Task.fromJson(taskJson));
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return storedTasks;
+    }
+
+    public void saveTasks(){
+        StringBuilder jsonBuilder = new StringBuilder();
+        jsonBuilder.append("[\n");
+        for (int i = 0; i < tasks.size(); i++){
+            jsonBuilder.append(tasks.get(i).toJson());
+            if (i < tasks.size() - 1){
+                jsonBuilder.append(",\n");
+            }
+        }
+        jsonBuilder.append("\n]");
+
+        try {
+            Files.writeString(FILE_PATH, jsonBuilder.toString());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void addTask(String description){
+        Task newTask = new Task(description);
+        tasks.add(newTask);
+        System.out.println("Task added successfully (ID: " + newTask.getId() + ")");
+    }
+
+    public void updateTask(String id, String new_description){
+        Task task = findTask(id).orElseThrow(() -> new IllegalArgumentException("Task with ID " + id + " not found!"));
+        task.updateDescription(new_description);
+    }
+
+    public void deleteTask(String id){
+        Task task = findTask(id).orElseThrow(() -> new IllegalArgumentException("Task with ID " + id + " not found!"));
+        tasks.remove(task);
+    }
+
+    public void markInProgress(String id){
+        Task task = findTask(id).orElseThrow(() -> new IllegalArgumentException("Task with ID " + id + " not found!"));
+        task.markInProgress();
+    }
+
+    public void markDone(String id){
+        Task task = findTask(id).orElseThrow(() -> new IllegalArgumentException("Task with ID " + id + " not found!"));
+        task.markDone();
+    }
+
+    public void listTasks(String type){
+        for (Task task : tasks){
+            String status = task.getStatus().toString().strip();
+            if (type.equals("All") || status.equals(type)){
+                System.out.println(task.toString());
+            }
+        }
+    }
+
+    public Optional<Task> findTask(String id) {
+        return tasks.stream().filter((task) -> task.getId() == Integer.parseInt(id)).findFirst();
+    }
+
+
+}
